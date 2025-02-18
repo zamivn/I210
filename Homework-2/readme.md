f03ad9b6d55a018eae764e0d6270b46054bbf4ee
zamivn <slayten050505@gmail.com>
2025-02-18 11:20:53 -0500

    here we go with java...

---
+++ Homework-2/readme.md
@@ -0,0 +1,11 @@
+## Assignment 1: Git Readme
+
+### Blog Topic Summary
+
+On this blog i will be discussing my favorite breed of dog
+
+### Key Takeaways
+
+### Live Link
+
+https://github.com/zamivn/homework-2-blog.git
---
+++ week-6-1/js/external.js
@@ -0,0 +1,11 @@
+console.log("Umm...Where are we?");
+const birthYear= 1207;
+let myName = "Ty the noodle farm guy";
+console.log(myName, "was born", birthYear)
+myName = "Ty the Fly Guy";
+//constant variables cant change value
+// birthYear = 1208;
+console.warn(myName, "May have devoured 10 souls");
+console.log("myName is", myName.length, "character long");
+console.log("myName includes ty",myName.toLowerCase().includes("TY"));
+console.log("myName has uppercase:", myName!== myName.toLowerCase());
